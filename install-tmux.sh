3031c67bc4af96edacc55ec27f6047bcbe738fde
Yiwen Song <songzgy@gmail.com>
2019-04-01 02:00:12 -0700

    Create install-tmux.sh

---
+++ install-tmux.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+VERSION=2.8
+pushd /tmp
+wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz -O tmux.tar.gz
+tar -xvzf tmux.tar.gz
+cd tmux
+./configure && make
+make install
